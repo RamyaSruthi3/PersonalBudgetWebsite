97f0981dc0d2d0f79348f7cfb296c8dedf786b74
Ramya <rreddy11@uncc.edu>
2023-12-11 14:47:56 -0500

    Final Project

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,16 +5,18 @@ import CustomizedInput from "../components/shared/CustomizedInput";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+
 const Signup = () => {
   const navigate = useNavigate();
   const auth = useAuth();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const name = formData.get("name")
-    const email = formData.get("email")
-    const password = formData.get("password")
-    try {  
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    try {
       toast.loading("Signing Up", { id: "signup" });
       await auth?.signup(name, email, password);
       toast.success("Signed Up Successfully", { id: "signup" });
@@ -23,64 +25,68 @@ const Signup = () => {
       toast.error("Signing Up Failed", { id: "signup" });
     }
   };
+
   useEffect(() => {
     if (auth?.user) {
-      return navigate("/dashboard");
+      navigate("/dashboard");
     }
-  }, [auth]);
+  }, [auth, navigate]);
+
   return (
-    <Box width={"100%"} height={"100%"} >
-      
-        <form
-          onSubmit={handleSubmit}
-          style={{
-            borderRadius: "10px",
-            border: "none",
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center"
+    <Box width={"100%"} height={"100%"}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          borderRadius: "10px",
+          border: "none",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-            }}
+          <Typography
+            variant="h4"
+            textAlign="center"
+            padding={2}
+            fontWeight={600}
           >
-            <Typography
-              variant="h4"
-              textAlign="center"
-              padding={2}
-              fontWeight={600}
-            >
-              Signup
-            </Typography>
-            <CustomizedInput type="text" name="name" label="Name" />
-            <CustomizedInput type="email" name="email" label="Email" />
-            <CustomizedInput type="password" name="password" label="Password" />
-            <Button
-              type="submit"
-              sx={{
-                px: 2,
-                py: 1,
-                mt: 2,
-                width: "400px",
-                borderRadius: 2,
-                bgcolor: "#4d5791",
-                color : "white",
-                ":hover": {
-                  bgcolor: "white",
-                  color: "black",
-                },
-              }}
-              endIcon={<IoIosLogIn />}
-            >
-              Signup
-            </Button>
-          </Box>
-        </form>
-      </Box>
+            Signup
+          </Typography>
+          <CustomizedInput type="text" name="name" label="Name" />
+          <CustomizedInput type="email" name="email" label="Email" />
+          <CustomizedInput type="password" name="password" label="Password" />
+          <Typography variant="caption" sx={{ mt: 1, mb: 2, color: 'gray' }}>
+            Password must be at least 6 characters long
+          </Typography>
+          <Button
+            type="submit"
+            sx={{
+              px: 2,
+              py: 1,
+              mt: 2,
+              width: "400px",
+              borderRadius: 2,
+              bgcolor: "#4d5791",
+              color: "white",
+              ":hover": {
+                bgcolor: "white",
+                color: "black",
+              },
+            }}
+            endIcon={<IoIosLogIn />}
+          >
+            Signup
+          </Button>
+        </Box>
+      </form>
+    </Box>
   );
 };
 
